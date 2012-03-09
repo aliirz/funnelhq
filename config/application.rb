075ba10c85c146c6d276631a8d905dec1c41d743
Owain Lewis <owain@owainlewis.com>
2012-03-09 22:10:18 +0000

    moved fonts to asset path

--- config/application.rb
+++ config/application.rb
@@ -45,7 +45,8 @@ module Baseapp
 
     # Enable the asset pipeline
     config.assets.enabled = true
-
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
+  
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     
